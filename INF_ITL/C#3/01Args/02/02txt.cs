3d437ef167d6140bde11dd74e9d06cb6ee875a63
Andreas Ranzmaier <a.ranzmaier@barbaric.at>
2022-03-02 09:53:08 +0100

    adding a c# folder and finishing the first exercise

---
+++ INF_ITL/C#3/01Args/02/02txt.cs
@@ -0,0 +1,24 @@
+﻿// Working with Text file
+
+using System;
+using System.Collections.Generic;
+
+public class txt02
+{
+    public static void Main()
+    {
+        string[] lines = System.IO.File.ReadAllLines(@"../../../artikel.txt");
+        SortedDictionary<int, string> foo = new SortedDictionary<int, string>();
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string[] boo = lines[i].Split(' ');
+            foo.Add(int.Parse(boo[0]), boo[1]);
+        }
+
+        foreach (var i in foo)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ INF_ITL/C#3/01Args/03exists/03exists.cs
@@ -0,0 +1,42 @@
+﻿// existing?
+
+using System;
+
+public class existing03
+{
+    public static void Main()
+    {
+        string fileName = @"../../../artikellist.txt";
+        if (!File.Exists(fileName))
+        {
+            writingFile();
+        }
+        else
+        {
+            Console.WriteLine("File allready exists wanna override it? J/N");
+            string i = Console.ReadLine();
+            if (i == "J" || i == "j")
+            {
+                Console.WriteLine("Die Datei artikelliste.txt wurde neu erstellt.");
+                writingFile();
+            }
+            if (i == "N" || i == "n")
+            {
+                Console.WriteLine("Das Programm wird ohne Änderung beendet!");
+            }
+        }
+
+        void writingFile()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"../../../artikel.txt");
+            using (StreamWriter sw = File.CreateText(fileName))
+            {
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    sw.WriteLine(lines[i]);
+                }
+            }
+            Console.WriteLine("Die Datei artikelliste.txt wurde erfolgreich geschrieben");
+        }
+    }
+}
