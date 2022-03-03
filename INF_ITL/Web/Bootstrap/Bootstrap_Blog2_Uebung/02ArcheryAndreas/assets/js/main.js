d7124cd8df2bcb969289f75a8103b8a506d7700b
Andreas Ranzmaier <a.ranzmaier@barbaric.at>
2022-03-03 09:17:42 +0100

    adding a themeselect logic to resposively check when a select is changed

--- INF_ITL/Web/Bootstrap/Bootstrap_Blog2_Uebung/02ArcheryAndreas/assets/js/main.js
+++ INF_ITL/Web/Bootstrap/Bootstrap_Blog2_Uebung/02ArcheryAndreas/assets/js/main.js
@@ -10,26 +10,41 @@
   /**
    * Table Darstellung laut https://datatables.net/extensions/fixedheader/examples/integration/responsive-bootstrap.html
    */
-  $(document).ready(function () {
-    var table = $('#BlogbeitraegeTable').DataTable({
-      responsive: true
-    });
-    // todo: understand why this isn't needed
-    //new $.fn.dataTable.FixedHeader( table );
+ $(document).ready(function () {
+  var table = $('#BlogbeitraegeTable').DataTable({
+    responsive: true
   });
+});
 
-  // BlogbeitraegeTable, blogbeitraege
-  $("#meinSwitch").on("change", function () {
+// BlogbeitraegeTable, blogbeitraege
+$("#meinSwitch").on("change", function () {
 
-    if (this.checked == true) {
-      $("#Blog").show()
-      $("#blogbeitraege").hide()
-    }
-    else {
-      $("#Blog").hide()
-      $("#blogbeitraege").show()
-    }
-  })
+  if (this.checked == true) {
+    $("#Blog").show()
+    $("#blogbeitraege").hide()
+  }
+  else {
+    $("#Blog").hide()
+    $("#blogbeitraege").show()
+  }
+})
+
+// adding a Watch to see if font has changed 
+$("#themeselect").on("change", function () {
+  var value = $('#themeselect :selected').val();
+  console.log(value); 
+  switch (select) {
+      case 1:
+          
+          break;
+      case 2:
+
+          break;
+      case 3:
+        
+          break;
+  }
+})
   /**
    * Easy selector helper function
    */
