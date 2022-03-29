ecd316c8f1e9f8eacb342117065f35459112a165
Andreas Ranzmaier <a.ranzmaier@barbaric.at>
2022-03-29 20:23:50 +0200

    img with joins

--- INF_ITL/Datenbanken/00test_uebungen/adresse/01_mak_adresse_zmoa.sql
+++ INF_ITL/Datenbanken/00test_uebungen/adresse/01_mak_adresse_zmoa.sql
@@ -12,5 +12,19 @@ order by ort_name and str_name;
 -- 2)
 select ort_name 
 from ort
-left outer join plz using(plz_id)
-where plz_name is null;
+left outer join plz using(ort_id)
+where plz_nr is null;
+
+-- 3)
+select plz_nr as "PLZ"
+from plz 
+left outer join plz_strasse using(plz_id)
+where str_id is null;
+
+-- 4) group by ort_name having count(str_id) > 1
+select ort_name, str_id 
+from ort
+left join plz using(ort_id)
+left join plz_strasse using(plz_id)
+left join strasse using(str_id)
+group by ort_name;
